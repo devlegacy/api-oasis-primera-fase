74917734e3f3cb3bad68681e8a00d96b65322da6
Samuel R <samuel_-_rojas@hotmail.com>
2019-09-05 10:58:26 -0500

    Add hotel model

---
+++ app/Hotel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\ConsumptionCenter;
+
+class Hotel extends Model
+{
+    protected $table = 'hoteles';
+    // protected $fillable = [];
+    // protected $connection = '';
+    // protected $guarded = [];
+
+    public function consumptionCenter()
+    {
+        return $this->belongsToMany(ConsumptionCenter::class, 'centros_consumo_detalles', 'hotel_id', 'centro_consumo_id');
+    }
+
+    public static function getConsumptionCenterBy($id, $category)
+    {
+        return Self::with([
+        'consumptionCenter' => function ($query) use ($category) {
+            $query->with([
+              'schedules' => function ($query) {
+                  $query->where('dia', '=', getWeekDay())->orderBy('hora_inicio', 'ASC');
+              }
+            ])
+            ->where('categoria_id', $category)
+            ->has('schedules');
+        }
+      ])->findOrFail($id);
+    }
+}
