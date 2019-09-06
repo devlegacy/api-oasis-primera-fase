4197a3fa0d882ec7bd10376f74401267ced4760e
Samuel R <samuel_-_rojas@hotmail.com>
2019-09-06 02:47:45 -0500

    Add custom errors

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+trait ExceptionTrait
+{
+    public function apiException($request, Exception $e)
+    {
+        if ($this->isModel($e)) {
+            return $this->modelResponse();
+        }
+        if ($this->isHttp($e)) {
+            return $this->httpResponse();
+        }
+        return parent::render($request, $e);
+    }
+
+    protected function isModel(Exception $e)
+    {
+        return $e instanceof ModelNotFoundException;
+    }
+    protected function isHttp(Exception $e)
+    {
+        return $e instanceof NotFoundHttpException;
+    }
+    protected function modelResponse()
+    {
+        return response()->json(
+            [
+                'errors' => 'Model not found in API',
+            ],
+            Response::HTTP_NOT_FOUND
+        );
+    }
+    protected function httpResponse()
+    {
+        return response()->json(
+            [
+                      'errors' => 'Incorrect route',
+            ],
+            Response::HTTP_NOT_FOUND
+        );
+    }
+}
